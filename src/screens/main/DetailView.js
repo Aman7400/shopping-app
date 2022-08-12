dae4eb0548efee9473a614fed38b92c5dcd42fd5
Aman Shukla <amanks7400@gmail.com>
2022-08-12 12:32:02 +0530

    Update DetailView.js

--- src/screens/main/DetailView.js
+++ src/screens/main/DetailView.js
@@ -1,4 +1,4 @@
-import { Image, Text, View } from 'react-native'
+import { Image, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Icon from "react-native-vector-icons/Ionicons"
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -7,29 +7,88 @@ import { Button } from 'react-native-paper'
 
 const DetailView = ({ route, navigation }) => {
   const { index } = route.params
+  const [isLiked, setIsLiked] = React.useState(false)
+  const [selectedSize, setSelectedSize] = React.useState("S")
+  function handleSelectedSizeChange(size) {
+    setSelectedSize(size)
+  }
+
+  function handleBuyNow() {
+    let order = {
+      isLiked, selectedSize, price: index * 100, title: `Item ${index}`
+    }
+    alert(order)
+  }
+
   return (
-    <SafeAreaView style={{ backgroundColor: colors.background, flex: 1,padding: 32 }} >
-      <Icon name="arrow-back-circle-outline" onPress={() => navigation.goBack()} size={32} />
-      <Image style={{ height: 400, borderRadius: 32,marginTop:16,marginBottom:32 }} source={{ uri: "https://picsum.photos/200/700" }} />
+    <SafeAreaView style={{ backgroundColor: colors.background, flex: 1, padding: 24, paddingBottom: 0 }} >
+
+      {/* Toolbar */}
+      <DetailViewToolbar navigation={navigation} />
+      {/* Product Image */}
+      <Image style={{ height: 376, borderRadius: 32, marginTop: 16, marginBottom: 16 }} source={{ uri: "https://picsum.photos/200/700" }} />
+      {/* Product Details  */}
       <View style={{ flex: 1 }}>
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
           <Text style={{
             fontSize: 24,
             fontWeight: "bold",
           }}>DetailView {index}</Text>
-          <Text style={{
-            fontSize: 24,
-          }}> ${index * 100}</Text>
+          <Icon suppressHighlighting onPress={() => setIsLiked(!isLiked)} color={colors.primary} name={isLiked ? "heart" : "heart-outline"} size={30} />
         </View>
+        <Text style={{ marginVertical: 4, fontWeight: "bold", fontSize: 14 }}>About</Text>
         <Text style={{
-          fontSize: 16,
+          fontSize: 12,
           color: "grey",
         }}>lorem ipsum dolor sit amet, consectetur lorem3</Text>
-        <Button labelStyle={{ color: "white", fontWeight: "bold" }} style={{ marginTop: "auto", padding: 8, backgroundColor: colors.primary, borderRadius: 8 }}>
-          Buy Now
-        </Button>
+        <SelectSize selectedSize={selectedSize} handleSelectedSizeChange={handleSelectedSizeChange} />
+        <View style={{ marginTop: "auto", flexDirection: "row", alignItems: "center" }}>
+          <Text style={{ marginRight: 16, fontSize: 24, color: "grey", fontWeight: "bold" }}>
+            ${index * 100}
+          </Text>
+          <Button onPress={() => handleBuyNow()} labelStyle={{ flex: 1, color: "white", fontWeight: "bold" }} style={{ padding: 8, backgroundColor: colors.primary, borderRadius: 8, flex: 1 }}>
+            Buy Now
+          </Button>
+        </View>
       </View>
     </SafeAreaView>
+  )
+}
+
+function DetailViewToolbar({ navigation }) {
+  return (
+    <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+      <Icon suppressHighlighting name="arrow-back" onPress={() => navigation.goBack()} size={24} />
+      <Icon suppressHighlighting name="ellipsis-vertical-outline" onPress={() => alert("More Options")} size={24} />
+    </View>
+  )
+}
+
+function SelectSize({ handleSelectedSizeChange, selectedSize }) {
+
+  return (
+    <View style={{ marginVertical: 8 }}>
+      <Text style={{ marginBottom: 8, fontWeight: "bold", fontSize: 12 }}>Select Size</Text>
+      <View style={{ flexDirection: "row" }}>
+
+        {
+          ["S", "M", "L", "XL"].map((size, i) =>
+            <TouchableOpacity onPress={() => handleSelectedSizeChange(size)} style={{ marginLeft: 8 }} key={i} >
+              <Button
+                labelStyle={{
+                  fontWeight: "bold",
+                  color: selectedSize === size ? "white" : "grey"
+                }}
+                style={{
+                  backgroundColor: selectedSize === size ? colors.primary : 'white',
+                }} mode={selectedSize === size ? "contained" : "outlined"}>
+                {size}
+              </Button>
+            </TouchableOpacity>)
+        }
+      </View>
+    </View>
+
   )
 }
 
