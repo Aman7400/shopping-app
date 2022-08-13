56258fd14f4b6ffbc41cf8281ac98163c4d01874
Aman Shukla <amanks7400@gmail.com>
2022-08-13 10:44:41 +0530

    Update Notifications.js

--- src/screens/main/Notifications.js
+++ src/screens/main/Notifications.js
@@ -2,13 +2,17 @@ import { Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { colors } from '../../theme/theme'
+import Icon from "react-native-vector-icons/Ionicons"
 
-const Notifications = () => {
+const Notifications = ({navigation}) => {
     return (
-        <SafeAreaView style={{ flex: 1, padding: 32,backgroundColor: colors.background}}>
+        <SafeAreaView style={{ flex: 1, padding: 16, backgroundColor: colors.background }}>
             {/* Toolbar */}
             <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-                <Text style={{ fontSize: 24, fontWeight: "bold" }}>Notifications</Text>
+                <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+                    <Icon suppressHighlighting name="arrow-back" onPress={() => navigation.goBack()} size={24} />
+                    <Text style={{ marginLeft:16, fontSize: 24, fontWeight: "bold" }}>Notifications</Text>
+                </View>
                 <Text onPress={() => alert("Notifications")}>
                     Clear All
                 </Text>
