bee0418d33b3b4662634d88be62d5fa71d0b279b
Aman Shukla <amanks7400@gmail.com>
2022-08-13 10:31:44 +0530

    Add Cart Items Badge

--- src/navigations/Tabs.js
+++ src/navigations/Tabs.js
@@ -6,21 +6,28 @@ import Explore  from '../screens/main/Exlpore';
 import Cart  from '../screens/main/Cart';
 import Profile  from '../screens/main/Profile';
 import getTabIcon from '../utils/getTabIcon';
+import { CartContext } from '../contexts/CartContext';
+import { colors } from '../theme/theme';
 
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
+  const {addedItems} = React.useContext(CartContext)
   return (
     <Tab.Navigator screenOptions={({ route }) => ({
         tabBarIcon: (props) => getTabIcon(props, route),
         headerShown: false,
         tabBarShowLabel: false,
-        swipeEnabled:true
+        swipeEnabled:true,
+        tabBarBadgeStyle:{
+          backgroundColor:colors.primaryContainer,
+          color:"white"
+        }
     })}>
-      <Tab.Screen name="Home" component={HomeStack} />
-      <Tab.Screen name="Explore" component={Explore} />
-      <Tab.Screen name="Cart" component={Cart} />
+      <Tab.Screen name="Home"  component={HomeStack} />
+      <Tab.Screen name="Explore"  component={Explore} />
+      <Tab.Screen name="Cart" options={addedItems.length > 0 && {  tabBarBadge: addedItems?.length }} component={Cart} />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   )
